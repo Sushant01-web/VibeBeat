e66c1ee51cea0b2170f99891f00823ec5aee3194
SushantG <sg123004@gmail.com>
2025-09-23 23:58:17 +0530

    Welcome to VibeBeat, a sleek and modern web-based music player designed for seamless listening. With an intuitive interface and smooth controls, it lets you play, pause, skip, and organize your favorite tracks effortlessly.
    🌟 Features:
    
    🎧 Stream and enjoy high-quality music anytime
    
    📂 Create & manage custom playlists
    
    🔀 Shuffle, repeat, and queue tracks with ease
    
    🎵 Responsive design for desktop & mobile
    
    ⚡ Lightweight, fast, and user-friendly
    
    Whether you’re vibing to lo-fi beats, rocking out to classics, or exploring new genres, VubeBeat is your go-to destination for music that moves with you.
    
    🛠️ Tech Stack
    
    HTML5 – Structure of the player
    
    CSS3 – Styling with responsive design. Added Animation, Gradients, Transitions.
    
    JavaScript (ES6) – Core functionality and interactivity/ Focuses on conceptslikes Loops, DOM, Arrays and Evebts

---
+++ arjit.js
@@ -0,0 +1,587 @@
+/* 
+This Javascript Files Containes Codes and Logics for Below Listem Elements
+1. For Artists
+
+*/
+
+//Writing logic for scrolling artist left and right
+let pop_art_left = document.getElementById("pop_art_left")
+let pop_art_right = document.getElementById("pop_art_right")
+let artistItem = document.getElementsByClassName("artistItem")[0];
+
+//For moving artist to left direction by scliking on right arrow
+pop_art_right.addEventListener("click", ()=>{
+    artistItem.scrollLeft +=330;
+})
+
+//For moving artist to right direction by scliking on left arrow
+pop_art_left.addEventListener("click", ()=>{
+    artistItem.scrollLeft -=330;
+})
+
+
+
+//Writing All Logic for Masterplay where songs are actually play and controls
+const music = new Audio('Songs/AZUL.mp3')
+
+
+const songsobj = [
+    {
+        id: 1,
+        songName: `Chaleya - Jawan<br>
+        <div class="sub-title">Arjit Singh & Shilpa Rao</div>`,
+        poster: "Images/arjit/1.jpg"
+    },
+    {
+        id: 2,
+        songName: `Kasturi<br>
+        <div class="sub-title">Arjit Singh & Kunal Verma</div>`,
+        poster: "Images/arjit/2.jpg"
+    },
+    {
+        id: 3,
+        songName: `Bekhayali<br>
+        <div class="sub-title">Arjit Singh</div>`,
+        poster: "Images/arjit/3.jpg"
+    },
+    {
+        id: 4,
+        songName: `Channa Mereya<br>
+        <div class="sub-title">Arjit Singh</div>`,
+        poster: "Images/arjit/4.jpg"
+    },
+    {
+        id: 5,
+        songName: `Dhun<br>
+        <div class="sub-title">Arjit Singh</div>`,
+        poster: "Images/arjit/5.jpg"
+    },
+    {
+        id: 6,
+        songName: `O Maahi<br>
+        <div class="sub-title">Arjit Singh</div>`,
+        poster: "Images/arjit/6.jpg"
+    },
+    {
+        id: 7,
+        songName: `Pasoori Nu<br>
+        <div class="sub-title">Arjit Singh & Tulsi Kumar</div>`,
+        poster: "Images/arjit/7.jpg"
+    },
+    {
+        id: 8,
+        songName: `Tere Hawale<br>
+        <div class="sub-title">Shilpa Rao & Arjit Singh</div>`,
+        poster: "Images/arjit/8.jpg"
+    },
+    {
+        id: 9,
+        songName: `Ve Kamleyaa<br>
+        <div class="sub-title">Shreya Ghoshal & Arjit Singh</div>`,
+        poster: "Images/arjit/9.jpg"
+    },
+    {
+        id: 10,
+        songName: `Agar Tum Sath Ho<br>
+        <div class="sub-title">Arjit Singh and Alka Yagnik</div>`,
+        poster: "Images/arjit/10.jpg"
+    },
+    {
+        id: 11,
+        songName: `Kesariya<br>
+        <div class="sub-title">Arjit Singh</div>`,
+        poster: "Images/arjit/11.jpg"
+    },
+    {
+        id: 12,
+        songName: `Lutt Putt Gaya<br>
+        <div class="sub-title">Arjit Singh</div>`,
+        poster: "Images/arjit/12.jpg"
+    },
+    {
+        id: 13,
+        songName: `Satranga<br>
+        <div class="sub-title">Arjit Singh</div>`,
+        poster: "Images/arjit/13.jpg"
+    },
+    
+]
+
+
+let searchResult = document.getElementsByClassName("searchresult")[0]
+
+songsobj.forEach(element => {
+    const {id, songName, poster} = element
+    // console.log(poster)
+    let card = document.createElement("a")
+    card.classList.add('card')
+    card.href = '#' + id
+    card.innerHTML = `<img src="${poster}" alt="">
+                        <div class="content">
+                            ${songName}
+                        </div>`
+    searchResult.appendChild(card)
+});
+
+let input = document.getElementsByTagName('input')[0]
+input.addEventListener('keyup', ()=>{
+    let inputValue = input.value.toUpperCase()
+    let items = searchResult.getElementsByTagName('a')
+
+    for (let index = 0; index < items.length; index++) {
+        let as = items[index].getElementsByClassName('content')[0]
+        let textValue = as.textContent || as.innerHTML
+
+        if (textValue.toUpperCase().indexOf(inputValue) > -1) {
+            items[index].style.display = "flex"
+        } else {
+            items[index].style.display = "none"
+        }
+        
+        if (inputValue == 0) {
+            searchResult.style.display = "none"
+        } else {
+            searchResult.style.display = ""
+        }
+    }
+})
+
+
+
+//Getting Poster and Names and Singer of Songs
+Array.from(document.getElementsByClassName('songItem')).forEach((e, i)=>{
+    e.getElementsByTagName('img')[0].src = songsobj[i].poster
+    e.getElementsByTagName('h5')[0].innerHTML = songsobj[i].songName
+})
+
+//making logic to play songs and according to songs wave will act
+let masterPlay = document.getElementById("masterPlay")
+let wave = document.getElementById("wave")
+masterPlay.addEventListener("click", ()=>{
+    if (music.paused || music.currentTime <= 0) {
+        music.play()
+        wave.classList.add('active1')
+        masterPlay.classList.remove("bi-play-fill")
+        masterPlay.classList.add("bi-pause-fill")
+    }else{
+        music.pause()
+        wave.classList.remove('active1')
+        masterPlay.classList.add("bi-play-fill")
+        masterPlay.classList.remove("bi-pause-fill")
+    }
+})
+
+
+
+
+//Now changing play button to pause and vice verse
+const makeAllPlay = () =>{
+    Array.from(document.getElementsByClassName("playListPlay")).forEach((e)=>{
+        e.classList.remove("bi-play-fill")
+        e.classList.add("bi-pause-fill")
+        
+    })
+}
+
+//To Make background change when we select single song to play
+const makeAllBackground = () =>{
+    Array.from(document.getElementsByClassName("songItem")).forEach((e)=>{
+        e.style.background = 'rgb(105, 105, 105, .0)'
+        
+    })
+}
+
+
+
+
+
+//Targeting by id to each song to be play when they get click and changes its poster accorind to songs. also when got click master's play nd pause button get changed.
+let index = 0
+let posterMasterPlay = document.getElementById("posterMasterPlay")
+let downloadMusic = document.getElementById("downloadMusic")
+let title = document.getElementById('title')
+
+Array.from(document.getElementsByClassName('playListPlay')).forEach((e)=>{
+    e.addEventListener('click', (b)=>{
+        index = b.target.id
+        // console.log(index)
+        music.src = `Songs/arjit/${index}.mp3`;
+
+        //This Method for getting poster can also work but for more smooth to understand, new mwthod mention below in next 8line (posterMasterPlay.src = poster)
+        // posterMasterPlay.src = `ImagesMaster/${index}.jpg`;
+
+        music.play()
+        masterPlay.classList.remove("bi-play-fill")
+        masterPlay.classList.add("bi-pause-fill")
+        downloadMusic.href = `Songs/arjit/${index}.mp3`
+
+        //Adding songs title into master by filtering above array which consists of objects.
+        let songTitles = songsobj.filter((songElement)=>{
+            return songElement.id ==index
+        })
+
+        songTitles.forEach(element =>{
+            let {songName , poster} = element;
+            title.innerHTML = songName
+            posterMasterPlay.src = poster
+            downloadMusic.setAttribute('download', songName)//Through this line.. we can download our playing song
+        })
+
+        //function for when particular song get played its background will change
+        makeAllBackground()
+        document.getElementsByClassName("songItem")[index-1].style.background = "rgb(105, 105, 105, .1)";
+
+
+        //function for when we click on play button it will get played and alse wave will play
+        makeAllPlay();
+        e.classList.add('bi-play-fill')
+        e.classList.remove('bi-pause-fill')
+        wave.classList.add('active1')
+    })
+})
+
+
+
+//Writing Codes and Logics for Song's Time, progress bar
+let currentStart = document.getElementById("currentStart")
+let currentEnd = document.getElementById("currentEnd")
+let seek = document.getElementById("seek")
+let bar2 = document.getElementById("bar2") 
+let dot = document.getElementById("dot")
+
+
+music.addEventListener('timeupdate', ()=>{
+    let music_CurrentTime = music.currentTime;
+    let music_Duration = music.duration;
+    // console.log(music_Duration)
+
+    //Converting time into minutes
+    let minutes1 = Math.floor(music_Duration / 60)
+    let seconds1 = Math.floor(music_Duration % 60)
+
+    if (seconds1 < 10) {
+        seconds1 = `0${seconds1}`
+    }
+    currentEnd.innerHTML = `${minutes1}:${seconds1}`
+
+
+    let minutes2 = Math.floor(music_CurrentTime / 60)
+    let seconds2 = Math.floor(music_CurrentTime % 60)
+    if (seconds2 <= 9) {
+        seconds2 = `0${seconds2}`
+    }
+    currentStart.innerHTML = `${minutes2}:${seconds2}`
+
+
+    //writing codes and logic for progressbar
+    let progressbar = parseInt((music_CurrentTime / music_Duration)* 100)
+    seek.value = progressbar
+    // console.log(seek.value)
+    let seekbar = seek.value
+    bar2.style.width = `${seekbar}%`
+    dot.style.left = `${seekbar}%`  
+})
+
+
+
+//Writing Code and Logic to change points on seekbar wherever we want to play the currentsong
+seek.addEventListener('change', ()=>{
+    music.currentTime = seek.value * music.duration / 100;
+})
+
+
+
+/*All Buttons Logic and Codes Starts from Here*/
+//Writing Code and Logic for Volume
+let vol_icon = document.getElementById("vol_icon")
+let vol = document.getElementById("vol")
+let volBar = document.getElementById("volBar")
+let volDot = document.getElementById("volDot")
+
+vol.addEventListener("change", ()=>{
+    if (vol.value == 0) {
+        vol_icon.classList.remove("bi-volume-up");
+        vol_icon.classList.remove("bi-volume-down-fill")
+        vol_icon.classList.add("bi-volume-mute-fill")
+    }
+    if (vol.value > 0) {
+        vol_icon.classList.remove("bi-volume-up");
+        vol_icon.classList.add("bi-volume-down-fill")
+        vol_icon.classList.remove("bi-volume-mute-fill")
+    }
+    if (vol.value > 50) {
+        vol_icon.classList.add("bi-volume-up");
+        vol_icon.classList.remove("bi-volume-down-fill")
+        vol_icon.classList.remove("bi-volume-mute-fill")
+    }
+    //Writing code for controlling volume (Increase or Decrease)
+    let vol_a = vol.value
+    volBar.style.width = `${vol_a}%`
+    volDot.style.left = `${vol_a}%`
+    music.volume = vol_a / 100;
+})
+
+
+
+//Writing Logic and Code for Music Button to Shuffle, Random and Repeat
+let shuffle = document.getElementsByClassName("shuffle")[0]
+shuffle.addEventListener("click", ()=>{
+    let a = shuffle.innerHTML
+
+    switch (a) {
+        case "next":
+            shuffle.classList.add("bi-repeat")
+            shuffle.classList.remove("bi-music-note-beamed")
+            shuffle.classList.remove("bi-shuffle")           
+            shuffle.innerHTML = "repeat"
+            break;
+    
+        case "repeat":
+            shuffle.classList.remove("bi-repeat")
+            shuffle.classList.remove("bi-music-note-beamed")
+            shuffle.classList.add("bi-shuffle")           
+            shuffle.innerHTML = "random"
+            break;
+
+        case "random":
+            shuffle.classList.remove("bi-repeat")
+            shuffle.classList.add("bi-music-note-beamed")
+            shuffle.classList.remove("bi-shuffle")           
+            shuffle.innerHTML = "next"
+            break;
+    }
+})
+
+
+
+//Creating Function to Play Next Song Automatically when current song ends.
+const nextSong = ()=>{
+       if (index == songsobj.length) {
+        index = 1;
+    } else {
+        index++
+    }
+        music.src = `Songs/arjit/${index}.mp3`;
+
+        music.play()
+        masterPlay.classList.remove("bi-play-fill")
+        masterPlay.classList.add("bi-pause-fill")
+        downloadMusic.href = `Songs/arjit/${index}.mp3`
+
+        //Adding songs title into master by filtering above array which consists of objects.
+        let songTitles = songsobj.filter((songElement)=>{
+            return songElement.id ==index
+        })
+
+        songTitles.forEach(element =>{
+            let {songName , poster} = element;
+            title.innerHTML = songName
+            posterMasterPlay.src = poster
+            downloadMusic.setAttribute('download', songName)//Through this line 196.. we can download our playing song
+        })
+
+        //function for when particular song get played its background will change
+        makeAllBackground()
+        document.getElementsByClassName("songItem")[index-1].style.background = "rgb(105, 105, 105, .1)";
+
+
+        //function for when we click on play button it will get played and alse wave will play
+        makeAllPlay();
+        e.classList.add('bi-play-fill')
+        e.classList.remove('bi-pause-fill')
+        wave.classList.add('active1')
+}
+
+
+//Creating Function to Play Repeat Song.
+const repeatSong = ()=>{
+    index;
+        music.src = `Songs/arjit/${index}.mp3`;
+
+        music.play()
+        masterPlay.classList.remove("bi-play-fill")
+        masterPlay.classList.add("bi-pause-fill")
+        downloadMusic.href = `Songs/${index}.mp3`
+
+        //Adding songs title into master by filtering above array which consists of objects.
+        let songTitles = songsobj.filter((songElement)=>{
+            return songElement.id ==index
+        })
+
+        songTitles.forEach(element =>{
+            let {songName , poster} = element;
+            title.innerHTML = songName
+            posterMasterPlay.src = poster
+            downloadMusic.setAttribute('download', songName)//Through this line 196.. we can download our playing song
+        })
+
+        //function for when particular song get played its background will change
+        makeAllBackground()
+        document.getElementsByClassName("songItem")[index-1].style.background = "rgb(105, 105, 105, .1)";
+
+
+        //function for when we click on play button it will get played and alse wave will play
+        makeAllPlay();
+        e.classList.add('bi-play-fill')
+        e.classList.remove('bi-pause-fill')
+        wave.classList.add('active1')
+}
+
+
+
+//Creating Function to Play random Song from our array.
+const randomSong = ()=>{
+    if (index == songsobj.length) {
+        index = 1;
+    } else {
+        index = Math.floor((Math.random()*songsobj.length) + 1)
+    };
+        music.src = `Songs/arjit/${index}.mp3`;
+
+        music.play()
+        masterPlay.classList.remove("bi-play-fill")
+        masterPlay.classList.add("bi-pause-fill")
+        downloadMusic.href = `Songs/${index}.mp3`
+
+        //Adding songs title into master by filtering above array which consists of objects.
+        let songTitles = songsobj.filter((songElement)=>{
+            return songElement.id ==index
+        })
+
+        songTitles.forEach(element =>{
+            let {songName , poster} = element;
+            title.innerHTML = songName
+            posterMasterPlay.src = poster
+            downloadMusic.setAttribute('download', songName)//Through this line 196.. we can download our playing song
+        })
+
+        //function for when particular song get played its background will change
+        makeAllBackground()
+        document.getElementsByClassName("songItem")[index-1].style.background = "rgb(105, 105, 105, .1)";
+
+
+        //function for when we click on play button it will get played and alse wave will play
+        makeAllPlay();
+        e.classList.add('bi-play-fill')
+        e.classList.remove('bi-pause-fill')
+        wave.classList.add('active1')
+}
+
+
+//Here we are adding event listner to music abt what to do when song end -- reference to music button, shuffle or repear
+music.addEventListener("ended", ()=>{
+    let b = shuffle.innerHTML
+
+    switch (b) {
+        case 'repeat':
+            repeatSong()
+            break;
+    
+        case 'next':
+            nextSong()
+            break;
+        case 'random':
+            randomSong()
+            break;
+    }
+    
+})
+
+
+
+
+//Writing Code and Logic for Next and Previous Buttons.
+let back = document.getElementById("back")
+let next = document.getElementById("next")
+
+//for Making Songs Previous
+back.addEventListener("click", ()=>{
+    index -= 1;
+    if (index < 1) {
+        index = Array.from(document.getElementsByClassName("songItem")).length
+    }
+    music.src = `Songs/arjit/${index}.mp3`;
+
+    music.play()
+    masterPlay.classList.remove("bi-play-fill") 
+    masterPlay.classList.add("bi-pause-fill")
+
+    //Adding songs title into master by filtering above array which consists of objects.
+    let songTitles = songsobj.filter((songElement)=>{
+        return songElement.id ==index
+    })
+
+    songTitles.forEach(element =>{
+        let {songName , poster} = element;
+        title.innerHTML = songName
+        posterMasterPlay.src = poster
+        })
+
+    //function for when particular song get played its background will change
+    makeAllBackground()
+    document.getElementsByClassName("songItem")[index-1].style.background = "rgb(105, 105, 105, .1)";
+
+
+    //function for when we click on play button it will get played and alse wave will play
+    makeAllPlay();
+    e.classList.add('bi-play-fill')
+    e.classList.remove('bi-pause-fill')
+    wave.classList.add('active1')
+})
+
+//For Making Songs Next
+next.addEventListener("click",()=>{
+    index ++ ;
+    if (index > Array.from(document.getElementsByClassName("songItem")).length) {
+        index = 1;
+    }
+
+    music.src = `Songs/arjit/${index}.mp3`;
+
+    music.play()
+    masterPlay.classList.remove("bi-play-fill") 
+    masterPlay.classList.add("bi-pause-fill")
+
+    //Adding songs title into master by filtering above array which consists of objects.
+    let songTitles = songsobj.filter((songElement)=>{
+        return songElement.id ==index
+    })
+
+    songTitles.forEach(element =>{
+        let {songName , poster} = element;
+        title.innerHTML = songName
+        posterMasterPlay.src = poster
+        })
+
+    //function for when particular song get played its background will change
+    makeAllBackground()
+    document.getElementsByClassName("songItem")[index-1].style.background = "rgb(105, 105, 105, .1)";
+
+
+    //function for when we click on play button it will get played and alse wave will play
+    makeAllPlay();
+    e.classList.add('bi-play-fill')
+    e.classList.remove('bi-pause-fill')
+    wave.classList.add('active1')
+})
+
+
+
+
+//Writing Logic for Scrolling Songs left and right
+let pop_song_left = document.getElementById("pop_song_left")
+let pop_song_right = document.getElementById("pop_song_right")
+let popSongs = document.getElementsByClassName("popSongs")[0];
+
+//For moving songs to left direction by scliking on right arrow
+pop_song_right.addEventListener("click", ()=>{
+    popSongs.scrollLeft +=330;
+})
+
+//For moving songs to right direction by scliking on left arrow
+pop_song_left.addEventListener("click", ()=>{
+    popSongs.scrollLeft -=330;
+})
+
+
+
